28d3bfa366253de783f71ef817bef5bedb40b2d3
Ankit-0009 <ankitcode2511@gmail.com>
2024-08-24 11:12:04 +0530

    Changes for TechStack Card for mobile screen

--- src/pages/Tech Stack/components/TechStackCard.jsx
+++ src/pages/Tech Stack/components/TechStackCard.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const TechStackCard = ({tech}) => {
     const {name, img, type} = tech
     return (
-        <div className='bg-[#181818] w-3/12  rounded-xl md:flex hidden flex-col gap-10 border-2 border-[#383737]'>
-            <div className='flex justify-center items-center mt-14'>
-                <img src={img} className='md:w-20 md:h-20 lg:w-22 lg-22' alt="logo" />
+        <div className='bg-[#181818] md:w-3/12 w-10/12 rounded-xl flex md:flex-col flex-row md:gap-10 border-2 border-[#383737]'>
+            <div className='flex justify-center items-center md:mt-14 md:p-0 p-2'>
+                <img src={img} className='md:w-20 md:h-20 lg:w-22 lg-22 w-8 h-8' alt="logo" />
             </div>
-            <div className='flex flex-row justify-between mb-4'>
-                <h1 className='text-white font-medium ml-3'>{name}</h1>
-                <p className='text-[#808080] mr-3 '>{type}</p>
+            <div className='flex flex-row justify-between mb-4 md:mt-3 mt-4 w-full'>
+                <h1 className='text-white md:text-base text-sm font-medium ml-3'>{name}</h1>
+                <p className='text-[#808080] md:text-base text-xs mr-3 text-end'>{type}</p>
             </div>
         </div>       
     )
--- src/pages/Tech Stack/components/TechStackContainer.jsx
+++ src/pages/Tech Stack/components/TechStackContainer.jsx
@@ -3,16 +3,15 @@ import TechStackCard from './TechStackCard'
 import { stack } from '../../../utils/constant'
 
 const TechStackContainer = () => {
-  return (
-    <div className='mt-16 md:w-9/12 mx-auto flex md:flex-wrap justify-center items-center gap-5'>
-
-        {
-            stack.map((tech, index) => (
-                <TechStackCard key={index} tech={tech} />
-            ))
-        }
-    </div>
-  )
+    return (
+      <div className='mt-16 md:w-9/12 w-12/12 mx-auto flex flex-wrap justify-center items-center gap-6'>
+          {
+              stack.map((tech, index) => (
+                  <TechStackCard key={index} tech={tech} />
+              ))
+          }
+      </div>
+    )
 }
 
 export default TechStackContainer
